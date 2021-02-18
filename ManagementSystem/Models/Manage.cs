f142cd785d494762f3bf7f39f3a106221cf61033
HuynhQuangHuy <buyhuynh123@gmail.com>
2021-02-18 19:23:59 +0700

    Adding Manage Model

---
+++ ManagementSystem/Models/Manage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ManagementSystem.Models
+{
+    public class Manage
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+        public string Role { get; set; }
+        public decimal Age { get; set; }
+    }
+    public class ManageDBContext : DbContext
+    {
+        public DbSet<Manage> Manages { get; set; }
+    }
+}
